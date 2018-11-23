f8ec6ad2b95c9e0081ba55c7b55f62fa2aa65896
qgerman2 <qgerman2@gmail.com>
2018-11-22 23:55:38 -0300

    recursivo

--- src/main.js
+++ src/main.js
@@ -1,25 +1,41 @@
 var level;
+var y = 0;
 function preload() {
-    xml = loadXML("resources/levels/level_1.svg");
+    xml = loadXML("resources/levels/level_3.svg");
 }
 
 function setup() {
     canvas = createCanvas(800, 600);
     canvas.drawingContext.imageSmoothingEnabled = false;
     
-    textos = [[]];
-    populate(xml, textos[0]);
+    textos = [];
+    populate(xml, textos);
+    console.log("aver");
 }
 
 function populate(g, a) {
+    a.push(g.getName());
     if (g.hasChildren()) {
-        g.forEach(function(h) {
-            populate(h, a)
+        var i = a.push([]);
+        var c = g.getChildren();
+        c.forEach(function(h) {
+            populate(h, a[i - 1])
         })
     }
-    
+}
+
+function recursivedraw(a, l) {
+    if (typeof(a) == "string") {
+        text(a, l * 10, y);
+        y += 10;
+    }
+    else {
+        a.forEach(function(b) {
+            recursivedraw(b, l + 1);
+        })
+    }
 }
 
 function draw() {
-
+    recursivedraw(textos, 0);
 }
